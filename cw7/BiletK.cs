daf8998104b4db71707f18bd71e3dd57303a116e
Patryk Kapusto <murinus989@gmail.com>
2016-11-21 12:46:25 +0100

    cena biletu kolejowego

--- cw7/BiletK.cs
+++ cw7/BiletK.cs
@@ -11,7 +11,21 @@ namespace cw7
         private int dlugoscTrasy;
         public override void ObliczCene(RodzajBiletu rodzaj)
         {
-            
+            double cenaN;
+            if (dlugoscTrasy <= 100)
+                cenaN = dlugoscTrasy * 1.04;
+            else
+                cenaN = dlugoscTrasy * 0.73;
+            if (rodzaj == RodzajBiletu.N)
+                base.cena = cenaN;
+            else
+                base.cena = cenaN / 2;
+            Console.WriteLine(String.Format("{0:N2}", cena));
+        }
+
+        public BiletK(int odleglosc)
+        {
+            this.dlugoscTrasy = odleglosc;
         }
     }
 }
--- cw7/Program.cs
+++ cw7/Program.cs
@@ -12,8 +12,20 @@ namespace cw7
         static void Main(string[] args)
         {
             BiletA bilet1 = new BiletA();
+            BiletK bilet2 = new BiletK(100);
+            BiletK bilet3 = new BiletK(10);
+            BiletK bilet4 = new BiletK(200);
             bilet1.ObliczCene(RodzajBiletu.N);
             bilet1.ObliczCene(RodzajBiletu.U);
+            Console.WriteLine("-------------");
+            bilet2.ObliczCene(RodzajBiletu.N);
+            bilet2.ObliczCene(RodzajBiletu.U);
+            Console.WriteLine("-------------");
+            bilet3.ObliczCene(RodzajBiletu.N);
+            bilet3.ObliczCene(RodzajBiletu.U);
+            Console.WriteLine("-------------");
+            bilet4.ObliczCene(RodzajBiletu.N);
+            bilet4.ObliczCene(RodzajBiletu.U);
             Console.ReadKey();
         }
     }
