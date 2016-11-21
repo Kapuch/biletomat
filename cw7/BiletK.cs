eade70ddb7ec39c31b9df6b4f14eaad27ae70397
Patryk Kapusto <murinus989@gmail.com>
2016-11-21 12:29:26 +0100

    cena biletu autobusowego

---
+++ cw7/BiletK.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace cw7
+{
+    class BiletK:Bilet
+    {
+        private int dlugoscTrasy;
+        public override void ObliczCene(RodzajBiletu rodzaj)
+        {
+            
+        }
+    }
+}
--- cw7/Program.cs
+++ cw7/Program.cs
@@ -6,10 +6,15 @@ using System.Threading.Tasks;
 
 namespace cw7
 {
+    enum RodzajBiletu { N, U };
     class Program
     {
         static void Main(string[] args)
         {
+            BiletA bilet1 = new BiletA();
+            bilet1.ObliczCene(RodzajBiletu.N);
+            bilet1.ObliczCene(RodzajBiletu.U);
+            Console.ReadKey();
         }
     }
 }
